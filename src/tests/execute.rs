b30ee5721e6aec6b0b31295f3a06ec04aa68ff3d
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-11-20 14:35:39 -0500

    test: withdraw and withdraw_fee

--- src/tests/execute.rs
+++ src/tests/execute.rs
@@ -1,12 +1,14 @@
 use std::vec;
 
-use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+use cosmwasm_std::testing::{
+    mock_dependencies, mock_dependencies_with_balance, mock_env, mock_info,
+};
 use cosmwasm_std::{
     coins, from_binary, from_json, Addr, Api, BankMsg, CanonicalAddr, CosmosMsg, DepsMut, Env,
     MessageInfo, StdError, SubMsg, Uint128,
 };
 
-use crate::contract::{check, claim, deposit, instantiate, reward_all};
+use crate::contract::{check, claim, deposit, instantiate, reward_all, withdraw, withdraw_fee};
 use crate::msg::{
     CampaignCheckRequest, CampaignCheckResponse, InstantiateMsg, UserRewardRequest,
     UserRewardResponse,
@@ -352,5 +354,128 @@ fn test_check() {
             withdrawable_creation_fee: Uint128::new(170),
             claim_reward_fee: Uint128::new(999),
         }
+    );
+}
+
+#[test]
+fn test_withdraw() {
+    let mut deps = mock_dependencies_with_balance(&coins(1999, ""));
+    let env = mock_env();
+
+    instantiate(
+        deps.as_mut(),
+        env.clone(),
+        mock_info("creator", &[]),
+        InstantiateMsg {
+            claim_reward_fee: Some(Uint128::new(999)),
+        },
+    )
+    .unwrap();
+
+    deposit(
+        deps.as_mut(),
+        env.clone(),
+        mock_info("sender", &coins(1000, "")),
+        "test_campaign_1".to_string(),
+    )
+    .unwrap();
+
+    // reward user1
+    reward_all(
+        deps.as_mut(),
+        env.clone(),
+        mock_info("creator", &[]),
+        vec![UserRewardRequest {
+            campaign_id: "test_campaign_1".to_string(),
+            user_address: "user1".to_string(),
+            amount: Uint128::new(1000),
+        }],
+    )
+    .unwrap();
+
+    // try to claim from user1
+    claim(
+        deps.as_mut(),
+        env.clone(),
+        mock_info("user1", &coins(999, "")),
+        "test_campaign_1".to_string(),
+    )
+    .unwrap();
+
+    // try to withdraw
+    let resp = withdraw(
+        deps.as_mut(),
+        env.clone(),
+        mock_info("creator", &[]),
+        Uint128::new(999),
+    )
+    .unwrap();
+
+    assert_eq!(
+        resp.messages,
+        vec![SubMsg::new(CosmosMsg::Bank(BankMsg::Send {
+            to_address: "creator".to_string(),
+            amount: coins(999, ""),
+        }))]
+    );
+}
+
+#[test]
+fn test_withdraw_fee() {
+    let mut deps = mock_dependencies();
+    let env = mock_env();
+
+    instantiate(
+        deps.as_mut(),
+        env.clone(),
+        mock_info("creator", &[]),
+        InstantiateMsg {
+            claim_reward_fee: Some(Uint128::new(999)),
+        },
+    )
+    .unwrap();
+
+    deposit(
+        deps.as_mut(),
+        env.clone(),
+        mock_info("sender1", &coins(100, "")),
+        "test_campaign_1".to_string(),
+    )
+    .unwrap();
+
+    deposit(
+        deps.as_mut(),
+        env.clone(),
+        mock_info("sender2", &coins(200, "")),
+        "test_campaign_2".to_string(),
+    )
+    .unwrap();
+
+    // successful checks on two campaigns
+    check(
+        deps.as_mut(),
+        env.clone(),
+        mock_info("creator", &[]),
+        vec![
+            CampaignCheckRequest {
+                campaign_id: "test_campaign_1".to_string(),
+                amount: Uint128::new(50),
+            },
+            CampaignCheckRequest {
+                campaign_id: "test_campaign_2".to_string(),
+                amount: Uint128::new(80),
+            },
+        ],
+    )
+    .unwrap();
+
+    // withdraw fee
+    let resp = withdraw_fee(deps.as_mut(), env.clone(), mock_info("creator", &[])).unwrap();
+    assert_eq!(
+        resp.messages,
+        vec![SubMsg::new(CosmosMsg::Bank(BankMsg::Send {
+            to_address: "creator".to_string(),
+            amount: coins(170, ""),
+        }))]
     );
 }
