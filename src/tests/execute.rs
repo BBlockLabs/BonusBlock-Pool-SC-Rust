2518cd6665cb0230f8dd58aefce9184f8b735012
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-11-20 14:40:52 -0500

    test: set_refundable

--- src/tests/execute.rs
+++ src/tests/execute.rs
@@ -8,7 +8,9 @@ use cosmwasm_std::{
     MessageInfo, StdError, SubMsg, Uint128,
 };
 
-use crate::contract::{check, claim, deposit, instantiate, reward_all, withdraw, withdraw_fee};
+use crate::contract::{
+    check, claim, deposit, instantiate, reward_all, set_refundable, withdraw, withdraw_fee,
+};
 use crate::msg::{
     CampaignCheckRequest, CampaignCheckResponse, InstantiateMsg, UserRewardRequest,
     UserRewardResponse,
@@ -477,5 +479,51 @@ fn test_withdraw_fee() {
             to_address: "creator".to_string(),
             amount: coins(170, ""),
         }))]
+    );
+}
+
+#[test]
+fn test_set_refundable() {
+    let mut deps = mock_dependencies();
+    let env = mock_env();
+
+    instantiate(
+        deps.as_mut(),
+        env.clone(),
+        mock_info("creator", &[]),
+        InstantiateMsg {
+            claim_reward_fee: Some(Uint128::new(999)),
+        },
+    )
+    .unwrap();
+
+    deposit(
+        deps.as_mut(),
+        env.clone(),
+        mock_info("sender1", &coins(100, "")),
+        "test_campaign_1".to_string(),
+    )
+    .unwrap();
+
+    // withdraw fee
+    assert!(set_refundable(
+        deps.as_mut(),
+        env.clone(),
+        mock_info("creator", &[]),
+        "test_campaign_1".to_string(),
+    )
+    .is_ok());
+
+    // check campaign
+    let campaign = CAMPAIGN_POOL
+        .load(deps.as_ref().storage, "test_campaign_1".to_string())
+        .unwrap();
+    assert_eq!(
+        campaign,
+        Campaign {
+            amount: Uint128::new(100),
+            owner: Addr::unchecked("sender1"),
+            refundable: true,
+        }
     );
 }
