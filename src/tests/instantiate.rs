8bdd5373749da9ad4cfcd6f83b4f0453d968f889
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-11-20 08:20:16 -0500

    test: add instantiate tests

---
+++ src/tests/instantiate.rs
@@ -0,0 +1,50 @@
+use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+use cosmwasm_std::{from_slice, Addr, Api, DepsMut, OwnedDeps, Querier, Storage, SubMsg, Uint128};
+
+use crate::contract::{execute, instantiate, query};
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{State, CAMPAIGN_POOL, STATE, USER_POOL};
+
+#[test]
+fn test_instantiate_default() {
+    let mut deps = mock_dependencies();
+    let info = mock_info("sender", &[]);
+    let env = mock_env();
+    let msg = InstantiateMsg {
+        claim_reward_fee: None,
+    };
+
+    instantiate(deps.as_mut(), env.clone(), info.clone(), msg.clone()).unwrap();
+
+    let state = STATE.load(deps.as_ref().storage).unwrap();
+    assert_eq!(
+        state,
+        State {
+            owner: deps.api.addr_canonicalize("sender").unwrap(),
+            withdrawable_creation_fee: Uint128::zero(),
+            claim_reward_fee: Uint128::new(1000000000000000000),
+        }
+    );
+}
+
+#[test]
+fn test_instantiate_with_custom_claim_fee() {
+    let mut deps = mock_dependencies();
+    let info = mock_info("sender", &[]);
+    let env = mock_env();
+    let msg = InstantiateMsg {
+        claim_reward_fee: None,
+    };
+
+    instantiate(deps.as_mut(), env.clone(), info.clone(), msg.clone()).unwrap();
+
+    let state = STATE.load(deps.as_ref().storage).unwrap();
+    assert_eq!(
+        state,
+        State {
+            owner: deps.api.addr_canonicalize("sender").unwrap(),
+            withdrawable_creation_fee: Uint128::zero(),
+            claim_reward_fee: Uint128::new(1000000000000000000),
+        }
+    );
+}
